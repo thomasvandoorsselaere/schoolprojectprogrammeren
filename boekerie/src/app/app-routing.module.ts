76471335ad80675ede481f2011061aaebb20fe14
Programmeren\Thomas <vandoorsselaere.thomas@gmail.com>
2017-05-15 21:06:10 +0200

    routing

--- boekerie/src/app/app-routing.module.ts
+++ boekerie/src/app/app-routing.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { OverviewComponent } from 'app/overview/overview.component';
+import { DetailsComponent } from 'app/details/details.component';
+import { CreateComponent } from 'app/create/create.component';
 
 const routes: Routes = [
-  {
-    path: '',
-    children: []
-  }
+  { path: '',  pathMatch: 'full', redirectTo: 'overview'},
+  { path: 'overview', component: OverviewComponent},
+  { path: 'details', component: DetailsComponent},
+  { path: 'create', component: CreateComponent}
 ];
 
 @NgModule({
