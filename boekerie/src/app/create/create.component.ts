7994c0a195011ed3305a02479e5d53edbeab16ac
Programmeren\Thomas <vandoorsselaere.thomas@gmail.com>
2017-06-02 22:27:48 +0200

    Toevoegen van drop down bij create new book voor genre en auteurs

--- boekerie/src/app/create/create.component.ts
+++ boekerie/src/app/create/create.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { IBook, IGenre, IAuteur } from '../shared/index'
+import { AuteursService } from '../shared/auteurs.service'
+import { GenresService } from '../shared/genres.service'
 
 @Component({
   selector: 'app-create',
@@ -6,10 +9,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./create.component.css']
 })
 export class CreateComponent implements OnInit {
+  
+  auteurs: IAuteur[] = [];
+  genres: IGenre[] = [];
 
-  constructor() { }
+  selectedAuteur: IAuteur = undefined;
+  selectedGenre: IGenre = undefined;
+
+  constructor( 
+    private auteursService: AuteursService,
+    private genresService: GenresService
+    ) { }
 
   ngOnInit() {
+    this.auteursService.getAuteurs().subscribe(data => this.auteurs = data.value);
+    this.genresService.getGenres().subscribe(data =>  this.genres = data.value );
   }
 
 }
