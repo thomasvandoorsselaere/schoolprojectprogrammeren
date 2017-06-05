07e839bfc0c38d5100a4e078ffc306badcc028a2
Yoeri <yoeri.van.nieuwerburg@gmail.com>
2017-06-05 16:02:39 +0200

    NEEEEEEEEEEN, FORROOT!!!! :)

--- boekerie/src/app/app.module.ts
+++ boekerie/src/app/app.module.ts
@@ -64,7 +64,7 @@ import { UitgaveJaarPipe } from './shared/uitgave-jaar.pipe';
     HttpModule,
     AlertModule.forRoot(),
     RouterModule.forRoot(routes),
-    PopupModule
+    PopupModule.forRoot()
   ],
   providers: [
     BookService,
